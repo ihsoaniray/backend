8daa33436023ca1d32dac12cb47b02da29a8c98b
ihsoaniray <lunamei101010@protonmail.com>
2024-12-09 00:33:38 -0500

    Update server.js
    made connection more robust

--- server.js
+++ server.js
@@ -23,8 +23,14 @@ dotenv.config();
 require('./passport-setup');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+  .then(() => {
+    console.log('Connected to MongoDB...');
+  })
+  .catch((err) => {
+    console.error('Could not connect to MongoDB', err);
+    process.exit(1);  
+  });
+
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
